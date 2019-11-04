b9f65f10aad03b656e4f1809279f5604426ae118
Jose Aquino <joseaquino@logikode.tech>
2019-11-04 15:29:34 -0600

    Fixed a missing container wrapping for the main app file

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,19 @@ import React from 'react'
 import App from 'next/app'
 
 import '../assets/styles/app.scss'
+
 import BrandHeader from '../components/brandHeader'
 
 class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <BrandHeader />
-      <Component {...pageProps} />
-    )
-  }
+	render() {
+		const { Component, pageProps } = this.props
+		return (
+			<>
+				<BrandHeader />
+				<Component {...pageProps} />
+			</>
+		)
+	}
 }
 
 export default MyApp
