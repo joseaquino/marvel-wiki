32389f6b7050cdc1e2cea44c952e79bd90e13b23
Jose Aquino <joseaquino@logikode.tech>
2019-11-04 22:08:46 -0600

    Added typography and additional colors to app styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import App from 'next/app'
+import Head from 'next/head'
 
 import '../assets/styles/app.scss'
 
@@ -10,6 +11,9 @@ class MyApp extends App {
 		const { Component, pageProps } = this.props
 		return (
 			<>
+				<Head>
+					<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&display=swap" rel="stylesheet" />
+				</Head>
 				<BrandHeader />
 				<Component {...pageProps} />
 			</>
