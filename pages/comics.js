92360d2099bd6dbe491cd171a9d4ad8cf238af67
Jose Aquino <joseaquino@logikode.tech>
2019-11-06 00:27:08 -0600

    Added functionality to load more comics

--- pages/comics.js
+++ pages/comics.js
@@ -1,25 +1,40 @@
+import { useRef, useState } from 'react'
 import Head from 'next/head'
 
 import ComicCard from '../components/comicCard'
+import LoadMoreBtn from '../components/loadMoreBtn'
 
-import { getComicsList } from '../services/comics'
+import { getComicsList, getNextComicsPage } from '../services/comics'
 
-const ComicsPage = ({ comics }) => (
-	<>
-		<Head>
-			<title>List of Marvel comics.</title>
-		</Head>
-		<div className="main-container">
-			<div className="info-cards">
-				{
-					comics.map(comicProps => (
-						<ComicCard key={comicProps.id} {...comicProps} />
-					))
-				}
+const ComicsPage = ({ comics }) => {
+	const [comicsState, setComicsState] = useState(comics)
+	const currentPage = useRef(1)
+
+	const loadNextComicsPage = () =>
+		getNextComicsPage(currentPage.current)
+			.then(newComics =>
+				setComicsState(comicsState.concat(newComics))
+			)
+			.then(() => currentPage.current = currentPage.current + 1)
+
+	return (
+		<>
+			<Head>
+				<title>List of Marvel comics.</title>
+			</Head>
+			<div className="main-container">
+				<div className="info-cards">
+					{
+						comicsState.map((comicProps, idx) => (
+							<ComicCard key={idx} {...comicProps} />
+						))
+					}
+				</div>
+				<LoadMoreBtn loaderFunc={loadNextComicsPage} />
 			</div>
-		</div>
-	</>
-)
+		</>
+	)
+}
 
 ComicsPage.getInitialProps = async () => {
 	const comics = await getComicsList()
--- services/comics.js
+++ services/comics.js
@@ -1,18 +1,31 @@
 import marvelRequest from './marvelApi'
 
+const PER_PAGE = 20
+
+const pickComicValues = comic => ({
+			id: comic.id,
+			title: comic.title,
+			thumbnail: comic.thumbnail,
+			issueNumber: comic.issueNumber
+})
+
 export const getComicsList = async () => {
 	const response = await marvelRequest('/comics', {})
 	const data = await response.json()
 	let comics = []
 
 	if (data.data && data.data.results) {
-		comics = data.data.results.map(comic => ({
-			id: comic.id,
-			title: comic.title,
-			thumbnail: comic.thumbnail,
-			issueNumber: comic.issueNumber
-		}))
+		comics = data.data.results.map(pickComicValues)
 	}
 
 	return comics
 }
+
+export const getNextComicsPage = pageNumber =>
+	marvelRequest('/comics', { offset: pageNumber * PER_PAGE })
+		.then(response => response.json())
+		.then(comics => 
+			comics.data && comics.data.results
+			? comics.data.results.map(pickComicValues)
+			: []
+		)
