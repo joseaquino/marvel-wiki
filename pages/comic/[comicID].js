715b1e5d5ce75e474277c6e9b71c633ba426597e
Jose Aquino <joseaquino@logikode.tech>
2019-11-07 08:11:45 -0600

    Added empty state handling for comic detail page

--- pages/comic/[comicID].js
+++ pages/comic/[comicID].js
@@ -6,10 +6,12 @@ import { getComic, getComicCharacters } from '../../services/comics'
 
 const ComicDetailPage = ({ comic }) => {
 	const [characters, setCharacters] = useState([])
+	const [charactersLoaded, setCharactersLoaded] = useState(false)
 
 	useEffect(() => {
 		getComicCharacters(comic.id)
-		.then(setCharacters)
+			.then(setCharacters)
+			.then(() => setCharactersLoaded(true))
 	}, [])
 
 	return (
@@ -21,20 +23,29 @@ const ComicDetailPage = ({ comic }) => {
 				</div>
 				<div className="detail-info">
 					<h1>{ comic.title }</h1>
-					<p>{ comic.description }</p>
+					{ 
+						comic.description === ''
+							? <span className="no-value">No description available for this comic book.</span>
+							: <p>{ comic.description }</p>
+					}
 					<div className="detail-divider" />
 					<h2>Characters</h2>
-					<div className="detail-characters">
-						{
-							characters.map((character, idx) => <CharacterCard key={idx} {...character} />)
-						}
-					</div>
+					{
+						charactersLoaded && characters.length === 0
+							? <span className="no-value">There are no characters listed for this comic book.</span>
+							: <div className="detail-characters">
+								{ characters.map((character, idx) => <CharacterCard key={idx} {...character} />) }
+							</div>
+					}
+					<div className="detail-divider" />
 					<h2>Stories</h2>
-					<ul>
-						{
-							comic.stories.map((story, idx) => <li key={idx}><a>{ story }</a></li>)
-						}
-					</ul>
+					{
+						comic.stories.length === 0
+							? <span className="no-value">There are no stories associated with this comic book</span>
+							: <ul>
+								{ comic.stories.map((story, idx) => <li key={idx}><a>{ story }</a></li>) }
+							</ul>
+					}
 				</div>
 			</div>
 		</div>
