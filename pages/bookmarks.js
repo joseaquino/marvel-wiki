9261e9a6b7b2d48b561199268c3f3ff7a57eee04
Jose Aquino <joseaquino@logikode.tech>
2019-11-07 12:34:40 -0600

    Added the bookmark page functionality and styling

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -1,13 +1,61 @@
 import Head from 'next/head'
 
-const BookmarksPage = () => (
-	<>
-		<Head>
-			<title>Bookmarked comics and characaters from the Marvel collection.</title>
-		</Head>
-		<h1>My bookmarks</h1>
-	</>
-)
+import { useState, useEffect } from 'react'
+
+import CharacterCard from '../components/characterCard'
+import ComicCard from '../components/comicCard'
+import ComicsIcon from '../components/icons/ComicsIcon'
+import CharactersIcon from '../components/icons/CharactersIcon'
+
+import { getBookmarks } from '../services/bookmarks'
+
+const BookmarksPage = () => {
+	const [activeBookmarks, setActiveBookmarks] = useState(null)
+	const [activeMenu, setActiveMenu] = useState('characters')
+	
+	useEffect(() => {
+		if (!activeBookmarks) setActiveBookmarks(getBookmarks())
+	}, [activeMenu])
+
+	return (
+		<>
+			<Head>
+				<title>Bookmarked comics and characaters from the Marvel collection.</title>
+			</Head>
+			<div className="main-container bookmarks">
+				<div className="bookmarks-side-nav">
+					<button
+						type="button"
+						onClick={() => setActiveMenu('characters')}
+						className={activeMenu === 'characters' ? 'active' : ''}
+					>
+						<CharactersIcon />
+						<span>Characters</span>
+					</button>
+					<button
+						type="button"
+						onClick={() => setActiveMenu('comics')}
+						className={activeMenu === 'comics' ? 'active' : ''}
+					>
+						<ComicsIcon />
+						<span>Comics</span>
+					</button>
+				</div>
+				<div className="bookmarks-container">
+					{
+						activeBookmarks
+						? activeBookmarks[activeMenu].map((bookmark, idx) => 
+							activeMenu === 'comics'
+							? <ComicCard key={idx} {...bookmark} />
+							: <CharacterCard key={idx} {...bookmark} />
+						)
+						: null
+					}
+				</div>
+			</div>
+		</>
+	)
+}
 
 export default BookmarksPage
 
