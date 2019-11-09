5dbfdcf42a72c6467af71f1d5ce27e6ff9fa154b
Jose Aquino <joseaquino@logikode.tech>
2019-11-09 11:45:13 -0600

    Added service worker configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,37 @@
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
 const withSass = require('@zeit/next-sass')
+const withOffline = require('next-offline')
+
+const swConfig = {
+	transformManifest: manifest => ['/'].concat(manifest),
+	workboxOpts: {
+		swDest: 'static/service-worker.js',
+		exclude: [/\.jpg$/, /\.png$/],
+		runtimeCaching: [
+			{
+				// Match any request that ends with .png, .jpg, .jpeg or .svg.
+				urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+				// Apply a cache-first strategy.
+				handler: 'CacheFirst',
+
+				options: {
+					// Use a custom cache name.
+					cacheName: 'images',
+
+					// Only cache 40 images.
+					expiration: {
+						maxEntries: 40,
+					},
+				}
+			}
+		]
+	}
+}
 
 const plugins = [
+	[withOffline, swConfig],
 	optimizedImages,
 	[withSass, {
 		sassLoaderOptions: {
@@ -10,4 +39,5 @@ const plugins = [
 		}
 	}]
 ]
+
 module.exports = withPlugins(plugins)
