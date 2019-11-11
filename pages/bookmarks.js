89f91d6079cb937da6b7d39a231c671c7a26c7ab
Jose Aquino <joseaquino@logikode.tech>
2019-11-11 10:43:00 -0600

    Made stored bookmarks clickable to their corresponding comic/character

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import { FixedSizeList as List } from 'react-window';
 
 import { useState, useEffect, useRef } from 'react'
@@ -9,22 +10,48 @@ import CharactersIcon from '../components/icons/CharactersIcon'
 import { getBookmarks, removeMarkedBookmarks } from '../services/bookmarks'
 
 const makeBookmarkListItem = (bookmarks, menu, removalAction, reverseRemoval) => ({ index, style }) =>
-	<div style={style} className={bookmarks[menu][index].markedForRemoval ? 'bookmark-item bookmark-removed' : 'bookmark-item'}>
+	<div
+		style={style}
+		className={
+			bookmarks[menu][index].markedForRemoval
+				? 'bookmark-item bookmark-removed'
+				: 'bookmark-item'
+		}>
 		<div className="bookmark-item-inner">
-			<div className="item-contents">
-				<span
-					className="item-image"
-					style={{ backgroundImage: `url(${bookmarks[menu][index].thumbnail})` }}
-				/>
-				<h1>{ menu === 'comics' ? bookmarks[menu][index].title : bookmarks[menu][index].name}</h1>
-			</div>
-			<div className="bookmark-actions">
-				{
-					bookmarks[menu][index].markedForRemoval
-						? <button type="button" className="item-undo" onClick={() => reverseRemoval(bookmarks[menu][index].id)}>Undo</button>
-						: <button type="button" className="item-remove" onClick={() => removalAction(bookmarks[menu][index].id)}>Remove</button>
-				}
-			</div>
+			<Link href={
+				menu === "characters"
+					? `/character/${bookmarks[menu][index].id}`
+					: `/comic/${bookmarks[menu][index].id}`
+			}>
+				<a className="item-contents">
+					<span
+						className="item-image"
+						style={{ backgroundImage: `url(${bookmarks[menu][index].thumbnail})` }}
+					/>
+					<h1>{
+						menu === 'comics'
+							? bookmarks[menu][index].title
+							: bookmarks[menu][index].name
+					}</h1>
+				</a>
+			</Link>
+			<div className="bookmark-actions">{
+				bookmarks[menu][index].markedForRemoval
+					? <button
+						type="button"
+						className="item-undo"
+						onClick={() => reverseRemoval(bookmarks[menu][index].id)}
+					>
+						Undo
+					</button>
+					: <button
+						type="button"
+						className="item-remove"
+						onClick={() => removalAction(bookmarks[menu][index].id)}
+					>
+						Remove
+					</button>
+			}</div>
 		</div>
 	</div>
 
@@ -84,20 +111,23 @@ const BookmarksPage = () => {
 						<span>Comics</span>
 					</button>
 				</div>
-				<div className="bookmarks-container">
-					{
-						activeBookmarks
-							? <List
-								height={384}
-								itemCount={activeBookmarks[activeMenu].length}
-								itemSize={84}
-								width="100%"
-							>
-								{ makeBookmarkListItem(activeBookmarks, activeMenu, markForRemoval, reverRemoval) }
-							</List>
+				<div className="bookmarks-container">{
+					activeBookmarks
+						? <List
+							height={384}
+							itemCount={activeBookmarks[activeMenu].length}
+							itemSize={84}
+							width="100%"
+						>{
+							makeBookmarkListItem(
+								activeBookmarks,
+								activeMenu,
+								markForRemoval,
+								reverRemoval
+							)
+						}</List>
 						: null
-					}
-				</div>
+				}</div>
 			</div>
 		</>
 	)
