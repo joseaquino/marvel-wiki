8a090a540311e85e659aa877f164ff563197e7e4
Jose Aquino <joseaquino@logikode.tech>
2019-11-14 14:15:17 -0600

    Updated the ComicsFilters to make use of the new FilterBoxTab data type

--- components/filters/ComicsFilters.js
+++ components/filters/ComicsFilters.js
@@ -1,12 +1,34 @@
 import React, { useState } from 'react'
 
-import FilterBoxBtn from '../buttons/filterBoxBtn'
+import FilterBoxBtn, { FilterBoxTab } from '../buttons/filterBoxBtn'
 import SortingBtn from '../buttons/sortingBtn'
 
 const tabs = [
-	{ queryKey: 'format', text: 'Format' },
-	{ queryKey: 'title', text: 'Title' },
-	{ queryKey: 'issueNumber', text: 'Issue #' }
+	FilterBoxTab.Dropdown.from({
+		label: 'Format',
+		queryKey: 'format',
+		value: '',
+		options: [
+			{ value: 'comic', label: 'Comic' },
+			{ value: 'magazine', label: 'Magazine' },
+			{ value: 'trade paperback', label: 'Trade Paperback' },
+			{ value: 'hardcover', label: 'Hardcover' },
+			{ value: 'digest', label: 'Digest' },
+			{ value: 'graphic novel', label: 'Graphic Novel' },
+			{ value: 'digital comic', label: 'Digital Comic' },
+			{ value: 'infinite comic', label: 'Infinite Comic' }
+		]
+	}),
+	FilterBoxTab.Text.from({
+		label: 'Title',
+		queryKey: 'title',
+		value: ''
+	}),
+	FilterBoxTab.Text.from({
+		label: 'Issue #',
+		queryKey: 'issueNumber',
+		value: ''
+	})
 ]
 
 const ComicsFilters = () => {
