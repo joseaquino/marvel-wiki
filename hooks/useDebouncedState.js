0539c1312308c49fecd0aac0fa8346c93ab45ad8
Jose Aquino <joseaquino@logikode.tech>
2019-12-19 18:07:23 -0600

    Fixed some linting issues

--- hooks/useDebouncedState.js
+++ hooks/useDebouncedState.js
@@ -4,15 +4,16 @@ const useDebouncedState = (initial, delay) => {
 	const [state, setState] = useState(initial)
 	const timeoutHandler = useRef(null)
 
-	if (typeof delay !== 'number')
+	if (typeof delay !== 'number') {
 		throw new TypeError('useStateDebounce: You must provide a number as a delay value')
+	}
 
-	const debounced = value => {
+	const debounced = (value) => {
 		clearTimeout(timeoutHandler.current)
 		timeoutHandler.current = setTimeout(() => setState(value), delay)
 	}
 
-	return [state, debounced]
+	return [state, debounced, setState]
 }
 
 export default useDebouncedState
