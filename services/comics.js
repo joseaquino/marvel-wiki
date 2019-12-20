485592bc1b7f447f9e22af40d6e345119710be2b
Jose Aquino <joseaquino@logikode.tech>
2019-12-19 18:56:40 -0600

    Resolved linting errors

--- services/comics.js
+++ services/comics.js
@@ -4,22 +4,22 @@ import { pickCharacterValues } from './characters'
 
 const PER_PAGE = 20
 
-export const pickComicValues = comic => ({
+export const pickComicValues = (comic) => ({
 	id: comic.id,
 	title: comic.title,
 	thumbnail: `${urlHttpToHttps(comic.thumbnail.path)}.${comic.thumbnail.extension}`,
 	issueNumber: comic.issueNumber,
 	description: comic.description,
-	stories: comic.stories.items.map(story => story.name)
+	stories: comic.stories.items.map((story) => story.name)
 })
 
-export const getComicsList = async query => {
+export const getComicsList = async (query) => {
 	const requestQuery = {
 		limit: PER_PAGE,
 		orderBy: query.sort === 'up' ? '-issueNumber' : 'issueNumber'
 	}
 
-	switch(query.filterBy) {
+	switch (query.filterBy) {
 		case 'format':
 			requestQuery.format = query.searchQuery
 			break
@@ -29,10 +29,12 @@ export const getComicsList = async query => {
 		case 'issueNumber':
 			requestQuery.issueNumber = query.searchQuery
 			break
+		default:
+			break
 	}
 
 	const response = await marvelRequest('/comics', requestQuery)
-	
+
 	const data = await response.json()
 	let comics = []
 
@@ -43,12 +45,12 @@ export const getComicsList = async query => {
 	return { comics, total: data.data ? data.data.total : Infinity }
 }
 
-export const getComic = async id => {
+export const getComic = async (id) => {
 	const response = await marvelRequest(`/comics/${id}`, {})
 	const comic = await response.json()
 
 	let comicDetails = {
-		stories: [],
+		stories: []
 	}
 
 	const results = comic.data && comic.data.results[0] ? comic.data.results[0] : null
@@ -60,29 +62,31 @@ export const getComic = async id => {
 	return comicDetails
 }
 
-export const getComicCharacters = id => {
-	return marvelRequest(`/comics/${id}/characters`, {})
-		.then(response => response.json())
-		.then(characters => characters.data && characters.data.results ? characters.data.results : null)
-		.then(results => {
-			let charactersDetails = []
+export const getComicCharacters = (id) => marvelRequest(`/comics/${id}/characters`, {})
+	.then((response) => response.json())
+	.then((characters) => (
+		characters.data && characters.data.results
+			? characters.data.results
+			: null
+	))
+	.then((results) => {
+		let charactersDetails = []
 
-			if (results)
-				charactersDetails = results.map(pickCharacterValues)
+		if (results) {
+			charactersDetails = results.map(pickCharacterValues)
+		}
 
-			return charactersDetails
-		})
+		return charactersDetails
+	})
 
-}
-
-export const getNextComicsPage = query => {
+export const getNextComicsPage = (query) => {
 	const requestQuery = {
 		offset: query.pageNumber * PER_PAGE,
 		orderBy: query.sort === 'up' ? '-issueNumber' : 'issueNumber',
 		limit: PER_PAGE
 	}
 
-	switch(query.filterBy) {
+	switch (query.filterBy) {
 		case 'format':
 			requestQuery.format = query.searchQuery
 			break
@@ -92,13 +96,15 @@ export const getNextComicsPage = query => {
 		case 'issueNumber':
 			requestQuery.issueNumber = query.searchQuery
 			break
+		default:
+			break
 	}
 
 	return marvelRequest('/comics', requestQuery)
-		.then(response => response.json())
-		.then(comics => 
+		.then((response) => response.json())
+		.then((comics) => (
 			comics.data && comics.data.results
-			? comics.data.results.map(pickComicValues)
-			: []
-		)
+				? comics.data.results.map(pickComicValues)
+				: []
+		))
 }
